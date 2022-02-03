df76355db62f4dd18d456e2380c41451f323dad3
yuvi76 <yuvraj.u@yudiz.in>
2022-02-03 09:31:29 +0530

    Updates

---
+++ precache-manifest.fe67c28e36699082a469892a5bb3f3f2.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca58d93f0e9240892854a0da7d04bbf1",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b6a5399bf22446553bf2",
+    "url": "/portfolio/static/css/main.33a2d7d8.chunk.css"
+  },
+  {
+    "revision": "0b1af867dc0862ffe07d",
+    "url": "/portfolio/static/js/2.594882ab.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/portfolio/static/js/2.594882ab.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b6a5399bf22446553bf2",
+    "url": "/portfolio/static/js/main.d74ccea5.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "845e7565239b353f9b7c39205f0ed949",
+    "url": "/portfolio/static/media/CryptoCurrency-Is-Proving-The-Best-Gauge-Of-The-Coronavirus-Outbreak-After-China-Stats-Shock.845e7565.jpg"
+  },
+  {
+    "revision": "c8c55cf9be88d8a460f4b5fbb481dcb8",
+    "url": "/portfolio/static/media/Mebook-Logo.c8c55cf9.PNG"
+  },
+  {
+    "revision": "555ce257911de0f838b14f3a0c366ff5",
+    "url": "/portfolio/static/media/bgyuvi.555ce257.jpg"
+  },
+  {
+    "revision": "f08384547028400008ce9a2a537aa199",
+    "url": "/portfolio/static/media/building-a-restful-API-with-node.js-and-mongoDB-Atlas.f0838454.jpg"
+  },
+  {
+    "revision": "cefcfb8b48059c06df4436fa7d98d026",
+    "url": "/portfolio/static/media/first-transaction-on-libra-blockchain.cefcfb8b.jpg"
+  },
+  {
+    "revision": "5e7ca51f32b00578911e7c3624d521ff",
+    "url": "/portfolio/static/media/logo2.5e7ca51f.png"
+  },
+  {
+    "revision": "7dff62e141dd754edba22809c0391ba8",
+    "url": "/portfolio/static/media/voting1-blog-banner.7dff62e1.jpg"
+  },
+  {
+    "revision": "dbe9ef39eb408df0447f768acb03211c",
+    "url": "/portfolio/static/media/yuvi.dbe9ef39.jpg"
+  }
+]);
